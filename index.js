cbc5143ed82b3ea62b2fb15d3282d384fe487cac
edikxl <edik24032002@gmail.com>
2020-06-09 15:25:19 +0300

    Added support for different screens for index, home and pronouns. Also pronouns still half way on.

--- index.js
+++ index.js
@@ -1,11 +1,71 @@
 function changePage( path ){
 
-  document.getElementById( 'inner-page' ).src = './pages/' + path + '/index.html'
+  $( '#inner-page' ).attr( 'src', './pages/' + path + '/index.html' );
+
+}
+
+function processNavButtonClicks(){
+
+  $menu = $( '#nav-menu' );
+  $nav = $( 'nav' );
+
+  $( '#nav-button' ).click( (e) => {
+
+    if( window.matchMedia('( orientation: portrait )').matches ){
+
+      if( $menu.css('display') == 'flex' ){
+
+        $menu.css( 'display', 'none' );
+        $nav.css( 'height', 'auto' );
+
+      }else{
+
+        $menu.css( 'display', 'flex' );
+        $nav.css( 'height', '100%' );
+
+      }
+
+      e.stopPropagation();
+
+    }
+
+  })
+
+  $( 'html' ).click( (e) => {
+
+    if( window.matchMedia('( orientation: portrait )').matches ){
+
+      if( e.target != $menu ){
+
+        $menu.css( 'display', 'none' );
+        $nav.css( 'height', 'auto' );
+
+      }
+
+    }
+
+  })
+
+  $( '#inner-page' ).on( 'load', () => {
+
+    $($( '#inner-page' )[0].contentWindow).on( 'click', (e) => {
+
+      if( window.matchMedia('( orientation: portrait )').matches ){
+
+        $menu.css( 'display', 'none' );
+        $nav.css( 'height', 'auto' );
+
+      }
+
+    });
+
+  });
 
 }
 
 window.onload = () => {
 
   changePage( 'home' )
+  processNavButtonClicks();
 
 }
