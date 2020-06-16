3f062d0cfae2095361daaeb1346a9621f877faf3
edikxl <edik24032002@gmail.com>
2020-06-16 18:41:37 +0300

    Added and filled 'Sentence Structure' page. Created script for template related stuff and so did automation of creation table of contents. Due to creating a script updated blank's and particles index.html's. I didn't touch other pages just because there is no need. Added title to all pages. Now it's easir to recognise them when I open them one by one in browser. Added some styles in template for faster building pages. Tried using svg for building diagrams, but decided to use grids only and here we are with table only...

---
+++ pages/template.js
@@ -0,0 +1,21 @@
+function goto(id){ document.location.assign(id); }
+
+$( document ).ready( () => {
+
+  var $table_of_contains = $( '#table-of-contains' );
+
+  $.each( $( '.title' ), ( index, elem ) => {
+
+    var id = $( elem ).text().toLowerCase().replace( ' ', '-' );
+
+    $( elem ).attr( 'id', id );
+
+    if( index != 0 ){ // Skiping first title ( main for the page )
+
+      $table_of_contains.append( $( '<div></div>' ).text( $( elem ).text() ).click( () => { goto( '#' + id ) } ) );
+
+    }
+
+  });
+
+});
